50bbc9abfbae1af0e576487c0bcb9287647371a8
venudi <venudidisara@gmail.com>
2023-09-17 18:21:18 +0530

    player move - jump, left, right

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // get player rigidbody
+    private Rigidbody2D player;
+    
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        D
+        player = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // move horizontally
+        player.velocity = new Vector2(Input.GetAxis("Horizontal") * 10, player.velocity.y);
+
+        // jump with button down
+        if (Input.GetButtonDown("Jump"))
+        {
+            player.velocity = new Vector2(player.velocity.x, 10);
+        }
     }
 }
