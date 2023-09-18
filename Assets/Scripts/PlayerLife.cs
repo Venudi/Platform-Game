809f42dbd3ef80c833695a86b15e7a6d2bb5c109
venudi <venudidisara@gmail.com>
2023-09-18 10:49:50 +0530

    restart level

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour
 {
+    // rigid body 2d
+    private Rigidbody2D rb;
     private Animator anim;
 
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
 
@@ -23,7 +27,15 @@ public class PlayerLife : MonoBehaviour
     // die
     private void Die()
     {
+        // set statice
+        rb.bodyType = RigidbodyType2D.Static;
         // set trigger
         anim.SetTrigger("death");
+    }
+    
+    // restart level
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
