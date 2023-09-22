3f8305d32e40f4f5e03663cd3eb709380867fc37
venudi <venudidisara@gmail.com>
2023-09-22 12:40:11 +0530

    lives

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour
 {
+    // player lives
+    public int lives = 5;
+    [SerializeField] private Text livesText;
+
     // rigid body 2d
     private Rigidbody2D rb;
     private Animator anim;
@@ -29,16 +33,34 @@ public class PlayerLife : MonoBehaviour
     // die
     private void Die()
     {
+        // reduce lives
+        lives--;
+        // play death sound
         deathSound.Play();
         // set statice
         rb.bodyType = RigidbodyType2D.Static;
         // set trigger
         anim.SetTrigger("death");
+        livesText.text = ": " + lives;
     }
     
     // restart level
     private void RestartLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        // go to game over screen if lives are 0
+        if (lives <= 0)
+        {
+            EndGame();
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    // end game
+    private void EndGame()
+    {
+        SceneManager.LoadScene("End Screen");
     }
 }
